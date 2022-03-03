256e115b707eeb80a8ee7d8940206cf10f28d1ea
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-03 19:40:14 +0700

    feat(components/sidebar.tsx): add button to github

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,6 +1,7 @@
 import { Box, Divider, Heading, HStack, Text, VStack } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/react";
 import { BiData } from "react-icons/bi";
-import { FaWeight } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import { GiWeightCrush } from "react-icons/gi";
 import { IconType } from "react-icons/lib";
 import { useNavigate } from "react-location";
@@ -42,9 +43,22 @@ const Sidebar = () => {
       p="8"
       flexShrink="0"
     >
-      <Heading as="h3" fontSize="3xl">
+      <Heading as="h3" fontSize="3xl" mb="2">
         Tirtawater
       </Heading>
+      <Button
+        leftIcon={<FaGithub />}
+        bg="white"
+        color="blue.400"
+        boxShadow="lg"
+      >
+        <a
+          target="_blank"
+          href="https://github.com/alfianandinugraha/tirtawater"
+        >
+          Download Source-code
+        </a>
+      </Button>
       <Divider my="4" />
       <Text textTransform="uppercase" opacity="75" mb="3" fontWeight="bold">
         Main
