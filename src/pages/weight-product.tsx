4c11fe8c7510efd153abd763bbc9660e9246fa5c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-03 15:45:54 +0700

    feat(pages/weight-product.tsx): add menu chip and init dataset

--- src/pages/weight-product.tsx
+++ src/pages/weight-product.tsx
@@ -1,10 +1,56 @@
+import Chip from "@/components/chip";
+import DataTable from "@/components/data-table";
+import { datasetColumns } from "@/data/columns";
+import dataset from "@/data/dataset";
 import DashboardLayout from "@/layouts/dashboard";
-import { Box } from "@chakra-ui/layout";
+import { HStack } from "@chakra-ui/layout";
+import { useState } from "react";
+
+const chipMenu = [
+  {
+    text: "Dataset",
+    value: "DATASET",
+  },
+  {
+    text: "Calculate Weight",
+    value: "CALCULATE",
+  },
+  {
+    text: "Normalize Weight",
+    value: "NORMALIZE",
+  },
+  {
+    text: "Search S",
+    value: "SEARCH",
+  },
+  {
+    text: "Ranking",
+    value: "RANKING",
+  },
+];
 
 const WeightProductPage = () => {
+  const [activeChip, setActiveChip] = useState(chipMenu[0]);
+
   return (
     <DashboardLayout title="Weight Product Method">
-      <Box>Hello</Box>
+      <HStack spacing="2" mb={3}>
+        {chipMenu.map((item) => {
+          return (
+            <Chip
+              key={item.value}
+              text={item.text}
+              active={item.value === activeChip.value}
+              onClick={() => {
+                setActiveChip(item);
+              }}
+            />
+          );
+        })}
+      </HStack>
+      {activeChip.value === "DATASET" ? (
+        <DataTable data={dataset} columns={datasetColumns} />
+      ) : null}
     </DashboardLayout>
   );
 };
