1e6dff0fccac566d837789b36a1fd99b4f22f3ce
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-03 20:29:20 +0700

    feat(pages/dataset.tsx): create button "Add Data"

--- src/pages/dataset.tsx
+++ src/pages/dataset.tsx
@@ -3,12 +3,21 @@ import { datasetColumns } from "@/data/columns";
 import dataset from "@/data/dataset";
 import useTitlePage from "@/hooks/use-title-page";
 import DashboardLayout from "@/layouts/dashboard";
+import { FiFilePlus } from "react-icons/fi";
+import { Button } from "@chakra-ui/react";
 
 const DatasetPage = () => {
   useTitlePage("Dataset");
 
   return (
-    <DashboardLayout title="Dataset">
+    <DashboardLayout
+      title="Dataset"
+      action={
+        <Button colorScheme="blue" leftIcon={<FiFilePlus />}>
+          Add Data
+        </Button>
+      }
+    >
       <DataTable data={dataset} columns={datasetColumns} />
     </DashboardLayout>
   );
