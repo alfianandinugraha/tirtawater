102517a935a37eed998675c3d5e40f0817824cc4
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-03 15:21:53 +0700

    feat(components): create DataTable from react-table

---
+++ src/components/data-table.tsx
@@ -0,0 +1,73 @@
+import { Box, HStack } from "@chakra-ui/layout";
+import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
+import { Column, useSortBy, useTable } from "react-table";
+import { HiArrowDown, HiArrowUp } from "react-icons/hi";
+
+type DataTableProps<T extends object> = {
+  data: T[];
+  columns: ReadonlyArray<Column<T>>;
+};
+
+function DataTable<T extends object>(props: DataTableProps<T>) {
+  const { getTableProps, headerGroups, getTableBodyProps, rows, prepareRow } =
+    useTable(
+      {
+        data: props.data,
+        columns: props.columns,
+      },
+      useSortBy
+    );
+
+  return (
+    <Box>
+      <Table {...getTableProps()}>
+        <Thead>
+          {headerGroups.map((headerGroup) => {
+            return (
+              <Tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => {
+                  return (
+                    <Th {...column.getHeaderProps()} w={column.width}>
+                      <Box {...column.getSortByToggleProps()}>
+                        <HStack display="flex" spacing="1">
+                          <Box>{column.render("Header")}</Box>
+                          {column.isSorted ? (
+                            <>
+                              {column.isSortedDesc ? (
+                                <HiArrowDown />
+                              ) : (
+                                <HiArrowUp />
+                              )}
+                            </>
+                          ) : null}
+                        </HStack>
+                      </Box>
+                    </Th>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <Tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <Td {...cell.getCellProps()} w={cell.column.width}>
+                      {cell.render("Cell")}
+                    </Td>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </Box>
+  );
+}
+
+export default DataTable;
