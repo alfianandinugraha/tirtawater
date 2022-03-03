3f36d811f69c44ac1ff2de5b9d078f4523f7d5c6
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-03 20:30:14 +0700

    feat(layouts/dashboard.tsx): add action props

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -1,10 +1,12 @@
 import Sidebar from "@/components/sidebar";
 import { Box, Heading, Text } from "@chakra-ui/layout";
+import { HStack } from "@chakra-ui/react";
 import React from "react";
 
 type DashboardLayoutProps = {
   children: React.ReactNode;
   title: string;
+  action?: React.ReactNode;
 };
 
 const DashboardLayout = (props: DashboardLayoutProps) => {
@@ -13,9 +15,12 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
       <Sidebar />
       <Box as="main" p="8" bg="gray.50" width="full">
         <Box bg="white" p="6" boxShadow="md" borderRadius="md">
-          <Heading as="h1" mb="4">
-            {props.title}
-          </Heading>
+          <HStack alignItems="center" justifyContent="space-between">
+            <Heading as="h1" mb="4">
+              {props.title}
+            </Heading>
+            {props.action}
+          </HStack>
           <Box>{props.children}</Box>
         </Box>
       </Box>
